8a81f61e7262d3e54b7c8d4592a19322b8e6917e
Mouad Malhoud <mouad-professionel@hotmail.com>
2023-10-23 22:47:40 -0400

    DONE: User class

---
+++ ToDoList/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+    public class User
+    {
+        private string username { get; set; }
+        private string email { get; set; }
+        private DateTime dateJoined { get; set; }
+        private string password { get; set; }
+
+        public User(string _username, string _email, DateTime _dateJoined, string _password)
+        {
+            username = _username;
+            email = _email;
+            dateJoined = _dateJoined;
+            password = _password;
+        }
+    }
+}
