c2b501b525041e3316724e514c779bd798e0682f
Mouad Malhoud <mouad-professionel@hotmail.com>
2023-10-28 23:11:04 -0400

    DONE:  Task class

--- ToDoList/Todo.Designer.cs
+++ ToDoList/Todo.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ToDoList
 {
-    partial class TodoPage
+    partial class Todo
     {
         /// <summary>
         /// Required designer variable.
@@ -31,57 +31,54 @@
             panel1 = new Panel();
             panel2 = new Panel();
             panel3 = new Panel();
-            lbl_nomUtilisateur = new Label();
+            lbl_nameUser = new Label();
             SuspendLayout();
             // 
             // panel1
             // 
-            panel1.BackColor = Color.DodgerBlue;
-            panel1.Location = new Point(57, 100);
+            panel1.BackColor = SystemColors.Highlight;
+            panel1.Location = new Point(68, 98);
             panel1.Name = "panel1";
-            panel1.Size = new Size(274, 525);
+            panel1.Size = new Size(276, 504);
             panel1.TabIndex = 0;
             // 
             // panel2
             // 
-            panel2.BackColor = Color.SandyBrown;
-            panel2.Location = new Point(408, 100);
+            panel2.BackColor = Color.Goldenrod;
+            panel2.Location = new Point(423, 98);
             panel2.Name = "panel2";
-            panel2.Size = new Size(274, 525);
+            panel2.Size = new Size(276, 504);
             panel2.TabIndex = 1;
             // 
             // panel3
             // 
-            panel3.BackColor = Color.PaleGreen;
-            panel3.Location = new Point(761, 100);
+            panel3.BackColor = Color.LimeGreen;
+            panel3.Location = new Point(783, 98);
             panel3.Name = "panel3";
-            panel3.Size = new Size(274, 525);
+            panel3.Size = new Size(276, 504);
             panel3.TabIndex = 1;
             // 
-            // lbl_nomUtilisateur
+            // lbl_nameUser
             // 
-            lbl_nomUtilisateur.AutoSize = true;
-            lbl_nomUtilisateur.Font = new Font("Lucida Console", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl_nomUtilisateur.ForeColor = SystemColors.ActiveCaptionText;
-            lbl_nomUtilisateur.Location = new Point(57, 30);
-            lbl_nomUtilisateur.Name = "lbl_nomUtilisateur";
-            lbl_nomUtilisateur.Size = new Size(117, 19);
-            lbl_nomUtilisateur.TabIndex = 2;
-            lbl_nomUtilisateur.Text = "Welcome, ";
+            lbl_nameUser.AutoSize = true;
+            lbl_nameUser.Font = new Font("Lucida Console", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lbl_nameUser.Location = new Point(21, 30);
+            lbl_nameUser.Name = "lbl_nameUser";
+            lbl_nameUser.Size = new Size(136, 21);
+            lbl_nameUser.TabIndex = 2;
+            lbl_nameUser.Text = "Welcome, ";
             // 
-            // TodoPage
+            // Todo
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.White;
-            ClientSize = new Size(1107, 652);
-            Controls.Add(lbl_nomUtilisateur);
+            ClientSize = new Size(1138, 626);
+            Controls.Add(lbl_nameUser);
             Controls.Add(panel3);
             Controls.Add(panel2);
             Controls.Add(panel1);
-            ForeColor = SystemColors.ActiveCaption;
-            Name = "TodoPage";
-            Text = "TodoPage";
+            Name = "Todo";
+            Text = "Todo";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -91,6 +88,6 @@
         private Panel panel1;
         private Panel panel2;
         private Panel panel3;
-        private Label lbl_nomUtilisateur;
+        private Label lbl_nameUser;
     }
 }
